b8a450731fcbba6d7e51562ab2471e42a5abbc06
Shri-AI-ML <Shri-AI-ML@users.noreply.github.com>
2026-01-26 14:11:38 +0530

    Changed user Query

--- src/retrieval.py
+++ src/retrieval.py
@@ -6,7 +6,7 @@ chunks = load_and_chunk("data/dummy.json")
 store = VectorStore()
 store.build_index(chunks)
 
-query = "What is FAISS used for?"
+query = "What is Machine Learning ?"
 results = store.search(query)
 
 for r in results:
